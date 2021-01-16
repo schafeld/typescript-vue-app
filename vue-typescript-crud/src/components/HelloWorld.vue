93301b777316b90d9f318437a0ea6ded6258a924
Oliver Schafeld <oliver@schafeld.com>
2021-01-16 16:38:05 +0100

    add Bootstrap

--- vue-typescript-crud/src/components/HelloWorld.vue
+++ vue-typescript-crud/src/components/HelloWorld.vue
@@ -27,6 +27,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <button class="m-3 btn btn-sm btn-danger">Danger, Will Robinson!</button>
   </div>
 </template>
 
